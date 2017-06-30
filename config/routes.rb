ca3b0557fc88be618d89e387307105131c6219d3
Christopher Ho <ho.christopher.tak@gmail.com>
2017-06-30 17:18:08 -0400

    added welcome controller and index and styled post#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
+  #defining the route
+  get 'welcome/index'
+  # sets the welcome controller index action to the route of our application
+  root 'welcome#index'
 end
