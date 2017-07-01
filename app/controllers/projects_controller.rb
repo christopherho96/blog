6b307ce9f3646a833949b994dcab89a4e80adfc4
Christopher Ho <ho.christopher.tak@gmail.com>
2017-07-01 10:56:41 -0400

    fixed error

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,50 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Project was succesfully saved"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Project succesfully updated"
+    else
+    render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.friendly.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :slug)
+  end
+
+end
