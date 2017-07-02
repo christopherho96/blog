207688533f139313614c7b32c67239e27787d39c
Christopher Ho <ho.christopher.tak@gmail.com>
2017-07-02 17:02:38 -0400

    please!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,17 +101,18 @@ Rails.application.configure do
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
-}
-
-config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
   }
-  :url =>':s3_domain_url',
-  :path => '/:class/:attachment/:id_partition/:style/:filename'
-}
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: 's3-eu-central-1.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename'
+  }
 end
