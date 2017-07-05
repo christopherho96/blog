9a3d4e2b8829585422885cb72bc00e2273f6677d
Christopher Ho <ho.christopher.tak@gmail.com>
2017-07-05 19:12:45 -0400

    added bootstrap gem file and edited layout of welcome page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
-  has_attached_file :image, styles: {large: "600x600", medium: "300x300>", thumb: "150x150>" },  validate_media_type: false
+  has_attached_file :image, styles: {large: "600x600", medium: "300x300", thumb: "150x150" },  validate_media_type: false
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
